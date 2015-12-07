a03ec40ae4c76c924b98b305a70a31929e1ea31a
yuin <yuin@inforno.net>
2015-12-07 16:41:39 +0900

    Now works on the linux platform

--- ip.lua
+++ ip.lua
@@ -2,30 +2,75 @@ local ibs = require("icebergsupport")
 local script_path = ibs.join_path(ibs.CONFIG_DIR, "luamodule")
 local icon = ibs.join_path(script_path, "ip", "ip.png")
 
+local function ip_windows()
+  local candidates = {}
+  local ok, stdout, stderr =  ibs.command_output([[ipconfig]])
+  if ok then
+    local text = ibs.crlf2lf(ibs.local2utf8(stdout))
+    local lines = ibs.regex_split("\r?\n", Regex.NONE, text)
+    local regname = Regex.new([[([^\s]{1}.*):]], Regex.NONE)
+    local regipv4 = Regex.new([[.*IPv4.*:\s*(.*)]], Regex.NONE)
+    local name = ""
+    for i, line in ipairs(lines) do
+      if regname:match(line) then
+        name = regname:_1()
+      end
+      if regipv4:match(line) then
+        table.insert(candidates, {value=regipv4:_1(), description=name, icon=icon})
+      end
+    end
+  end
+  return candidates
+end
+
+local function ip_linux()
+  local candidates = {}
+  local ok, stdout, stderr =  ibs.command_output([[ip addr show]])
+  if ok then
+    local text = ibs.crlf2lf(ibs.local2utf8(stdout))
+    local lines = ibs.regex_split("\r?\n", Regex.NONE, text)
+    local regname = Regex.new([[\d+\s*:\s*([^\s]+)\s*:(.*)]], Regex.NONE)
+    local regip = Regex.new([[\s+inet.*]], Regex.I)
+    for i, line in ipairs(lines) do
+      if regname:match(line) then
+        table.insert(candidates, {value=regname:_1(), description=regname:_2(), icon=icon})
+      elseif regip:match(line) then
+        table.insert(candidates, {value=line})
+      end
+    end
+  else
+    ok, stdout, stderr =  ibs.command_output([[/sbin/ifconfig]])
+    if ok then
+      local text = ibs.crlf2lf(ibs.local2utf8(stdout))
+      local lines = ibs.regex_split("\r?\n", Regex.NONE, text)
+      local regname = Regex.new([[([^\s]+)\s*(.*)]], Regex.NONE)
+      local regip = Regex.new([[\s+inet.*]], Regex.I)
+      for i, line in ipairs(lines) do
+        if regname:match(line) then
+          table.insert(candidates, {value=regname:_1(), description=regname:_2(), icon=icon})
+        elseif regip:match(line) then
+          table.insert(candidates, {value=line})
+        end
+      end
+    end
+  end
+  if ok then
+  end
+  return candidates
+end
+
 commands["ip"] = { 
   path = function(args) 
     if #args == 0 then return end
     ibs.set_clipboard(args[1])
   end, 
   completion = function(values)
-    local candidates = {}
-    local ok, stdout, stderr =  ibs.command_output([[ipconfig]])
-    if ok then
-      local text = ibs.crlf2lf(ibs.local2utf8(stdout))
-      local lines = ibs.regex_split("\r?\n", Regex.NONE, text)
-      local regname = Regex.new([[([^\s]{1}.*):]], Regex.NONE)
-      local regipv4 = Regex.new([[.*IPv4.*:\s*(.*)]], Regex.NONE)
-      local name = ""
-      for i, line in ipairs(lines) do
-        if regname:match(line) then
-          name = regname:_1()
-        end
-        if regipv4:match(line) then
-          table.insert(candidates, {value=regipv4:_1(), description=name, icon=icon})
-        end
-      end
+    local platform = ibs.build_platform()
+    if string.find(platform, "win") == 1 then
+      return ip_windows()
+    else
+      return ip_linux()
     end
-    return candidates
   end,
   description = "list current ip addresses",
   icon=icon,
